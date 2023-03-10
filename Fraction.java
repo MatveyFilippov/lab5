7f464c59dd701975a863812931a07d9196afc492
MatveyFilippov <mdfilippov_2@edu.hse.ru>
2023-03-10 11:51:50 +0300

    5 commit

--- Fraction.java
+++ Fraction.java
@@ -3,13 +3,13 @@ import java.util.regex.Pattern;
 
 class Ex_not_frac extends Exception{
     public Ex_not_frac(String print_err) {
-        System.out.println(print_err);
+        System.err.println(print_err);
     }
 }
 
 class Ex_here_nol extends Exception{
     public Ex_here_nol(){
-        System.out.println("Делить на ноль нельзя");
+        System.err.println("Делить на ноль нельзя");
     }
 }
 
@@ -112,22 +112,66 @@ class Manipulations {
         return a/b;
     }
 
-    public static double with_brackets (String input){
-        double answer = 0;
-        Pattern open = Pattern.compile("\\(");
-        Matcher open_brecket = open.matcher(input);
-        Pattern close = Pattern.compile("\\)");
-        Matcher close_brecket = close.matcher(input);
+    public static double with_brackets (String input) throws Ex_here_nol, Ex_not_frac {
+        double answer;
 
-        while (open_brecket.find() && close_brecket.find()){
-            System.out.println("Допиши этот раздел");
-            answer = 1;
+        Pattern open = Pattern.compile("\\(");
+        Pattern close = Pattern.compile("\\)");
+        Matcher open_bracket = open.matcher(input);
+        Matcher close_bracket = close.matcher(input);
+
+        StringBuilder time = new StringBuilder(input);
+
+        while (open_bracket.find() || close_bracket.find()){
+            open_bracket = open.matcher(time);
+            if (open_bracket.find()){
+                int start = open_bracket.end();
+                time.delete(start-1, start);
+            } else{
+                throw new Ex_not_frac("Скобка закрыта, но не открыта");
+            }
+            close_bracket = close.matcher(time);
+            if (close_bracket.find()){
+                int end = close_bracket.end();
+                time.delete(end-1, end);
+            } else{
+                throw new Ex_not_frac("Скобка открыта, но не закрыта");
+            }
+            open_bracket = open.matcher(time);
+            close_bracket = close.matcher(time);
         }
-        
+
+        time = new StringBuilder(input);
+        StringBuilder already = new StringBuilder(input);
+        open_bracket = open.matcher(time);
+        close_bracket = close.matcher(time);
+        int i = 0;
+        while (open_bracket.find() && close_bracket.find()){
+            i++;
+            int start = open_bracket.end();
+            int end = close_bracket.end();
+            int time_start = 0;
+            time.delete(0, start).delete(end-start-1, time.length());
+            open_bracket = open.matcher(time);
+            while (open_bracket.find()){
+                time_start = open_bracket.end();
+                time.delete(0, time_start);
+                open_bracket = open.matcher(time);
+            }
+            Fraction fraction_in_bracket = make_frac(infinity_string(String.valueOf(time)));
+            start = start + time_start;
+            already.replace(start-1, end, String.valueOf(fraction_in_bracket));
+            System.out.println(already);
+            close_bracket = close.matcher(already);
+            open_bracket = open.matcher(already);
+            time = new StringBuilder(already);
+        }
+        System.out.println(already);
+        answer = infinity_string(String.valueOf(already));
         return answer;
     }
 
-    public static double razdelitel (String input) throws Ex_here_nol, Ex_not_frac {
+    public static double infinity_string(String input) throws Ex_here_nol, Ex_not_frac {
         double answer;
         Pattern firs = Pattern.compile("^-?" + "[0-9]+" + "/" + "-?" + "[1-9]+" + "[0-9]*");
         Matcher get_first = firs.matcher(input);
@@ -153,7 +197,7 @@ class Manipulations {
                 if (get_next_frac.find()){
                     end = get_next_frac.end();
                     length = String.valueOf(another).length();
-                    time = another;
+                    time = new StringBuilder(another);
                     if ((length - end) > 0){
                         time.delete(end, length);
                     }
@@ -162,17 +206,17 @@ class Manipulations {
                     another.delete(0, end);
                 }
                 else{
-                    throw new Ex_not_frac("");
+                    throw new Ex_not_frac("Введено неверное выражение!");
                 }
             }
         }
         else{
-            throw new Ex_not_frac("");
+            throw new Ex_not_frac("Введено неверное выражение!");
         }
         return answer;
     }
 
-    private static double all_manipulations (String input) throws Ex_here_nol, Ex_not_frac {
+    private static double all_manipulations (String input) throws Ex_not_frac, Ex_here_nol {
         int something = input.indexOf(" ") + 1;
         int length = input.length();
 
--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-    public static void main(String[] args) throws Ex_here_nol {
+    public static void main(String[] args) throws Ex_here_nol, Ex_not_frac {
         Scanner in = new Scanner(System.in);
         System.out.print("Нажмите ENTER чтоб начать");
         String ENTER = in.nextLine();
@@ -11,16 +13,23 @@ public class Main {
             System.out.println();
             System.out.print("Введите выражение (например: -1/3 - 4/5 + 33/7) или stop, если хотите закончить: ");
             input = in.nextLine();
+            Pattern open = Pattern.compile("\\(");
+            Matcher open_bracket = open.matcher(input);
             try {
-                double answer = Manipulations.razdelitel(input);
+                double answer;
+                if(open_bracket.find()){
+                    answer = Manipulations.with_brackets(input);
+                }
+                else{
+                    answer = Manipulations.infinity_string(input);
+                }
                 Fraction frac = Manipulations.make_frac(answer);
                 System.out.println("Ответ: " + frac + " = " + answer);
             } catch(Ex_not_frac ex){
                 if (input.equals("stop")){
-                    System.out.println("Был рад поработать для вас!");
+                    System.out.println("Ой, сразу не понял, что на этом всё, был рад поработать для вас!");
                 }
                 else{
-                    System.out.println("Введено неверное выражение!");
                     System.out.println("Проверьте выражение на наличие лишних знаков или нехватки пробелов");
                 }
             }
